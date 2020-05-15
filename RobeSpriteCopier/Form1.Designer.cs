3e40eab9aebec9cda14799b35cb51f62a31a43f1
eni-ro <zZN93P6gktevUoyCYrAV>
2020-05-16 08:38:22 +0900

    Initial commit

---
+++ RobeSpriteCopier/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RobeSpriteCopier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(12, 12);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(197, 23);
+            this.buttonCopy.TabIndex = 0;
+            this.buttonCopy.Text = "copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 48);
+            this.Controls.Add(this.buttonCopy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RobeSpriteCopier";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCopy;
+    }
+}
+
---
+++ RobeSpriteCopier/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RobeSpriteCopier
+{
+    public partial class Form1 : Form
+    {
+        private string version = "0.1";
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text += " v" + version;
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ダイアログのタイトルを指定する
+            ofd.Title = "ファイルを選択して下さい";
+            ofd.Filter = "(*.spr)|*.spr|すべてのファイル(*.*)|*.*";
+
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string spr_path = ofd.FileName;
+
+
+            IEnumerable<string> files = Directory.EnumerateFiles(Path.GetDirectoryName(spr_path), "*", System.IO.SearchOption.AllDirectories);
+
+            //ファイルを列挙する
+            foreach (string f in files)
+            {
+                if(Path.GetExtension(f) == ".act")
+                {
+                    File.Copy(
+                        spr_path,
+                        Path.GetDirectoryName(f) + "\\" + Path.GetFileNameWithoutExtension(f) + ".spr"
+                        );
+                }
+            }
+            System.Media.SystemSounds.Asterisk.Play();
+        }
+    }
+}
